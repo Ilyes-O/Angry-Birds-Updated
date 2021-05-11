965cb139fb820bfad1b5c22ee6bd08202d5d0669
Ilyes Ouldsaada <ouldsaada.ilyes@gmail.com>
2021-05-11 16:59:14 -0400

    Disappering pigs

--- sketch.js
+++ sketch.js
@@ -17,7 +17,6 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -64,7 +63,7 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();  
 }
 
 function mouseDragged(){
@@ -74,4 +73,10 @@ function mouseDragged(){
 
 function mouseReleased(){
     slingshot.fly();
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(bird.body);
+    }
 }
